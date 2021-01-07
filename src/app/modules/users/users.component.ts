2275eec91a22cc94f002ecd2dd61de33854de38d
rohitVerma <rohitVerma@rohitverma.com>
2021-01-07 16:48:35 +0530

    FetchingDataFromRedux

--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { UserModel } from 'src/app/models/user.model';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -8,14 +10,17 @@ import { UsersService } from 'src/app/services/users.service';
   templateUrl: './users.component.html',
 })
 export class UsersComponent implements OnInit {
-  users: UserModel[];
+  // users: UserModel[];
+  allUsersState: Observable<{ users: UserModel[] }>;
 
-  constructor(private router: Router, private usersService: UsersService) {
+  constructor(private router: Router, private usersService: UsersService,
+    private store: Store<{ usersState: { users: UserModel[] } }>) {
 
   }
 
   ngOnInit(): void {
-    this.users = this.usersService.getAllUsers();
+    // this.users = this.usersService.getAllUsers();
+    this.allUsersState = this.store.select("usersState");
   }
 
   goToUserDetails(userId: string) {
