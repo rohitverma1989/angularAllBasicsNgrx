4554f2dc8d6ecc41dec3f8f25b97b7095e2311ee
rohitVerma <rohitVerma@rohitverma.com>
2021-01-14 00:46:27 +0530

    LAZYLoadedModuleNewSyntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { NotFoundComponent } from "./modules/not-found/not-found.component";
 import { ServersComponent } from "./modules/servers/servers.component";
+// import { ss } from "./modules";
 
 const appRoutes: Routes = [
   { path: "servers", component: ServersComponent },
-  { path: "admin", loadChildren: "./modules/admin/admin.module#AdminModule" },
+  // { path: "admin", loadChildren: "./modules/admin/admin.module#AdminModule" },
+  { path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(m => m.AdminModule) },
   // {
   //   path: "users", component: UsersComponent, children: [
   //     { path: ":id", component: UserComponent },
@@ -17,7 +19,9 @@ const appRoutes: Routes = [
 ]
 @NgModule({
   // imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+  ],
   exports: [RouterModule],
 
 })
